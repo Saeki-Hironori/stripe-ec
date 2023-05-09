b519c94af1062c59112af0a4dc61bf09c1f88d24
Saeki Hironori <saeki.hironori@itfllc.co.jp>
2023-05-09 16:21:11 +0900

    完成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,43 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { AppProps } from "next/app";
+import { Navbar, Container } from "react-bootstrap";
+import { CartProvider, DebugCart } from "use-shopping-cart";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <CartProvider
+      mode="payment"
+      cartMode="client-only"
+      stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_API_KEY}
+      currency="JPY"
+      successUrl="http://localhost:3000/success"
+      cancelUrl="http://localhost:3000"
+    >
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Hello EC</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Component {...pageProps} />
+      {process.env.NODE_ENV !== "production" ? (
+        <div
+          style={{
+            width: "30%",
+            position: "fixed",
+            bottom: 50,
+            right: 50,
+            overflowX: "scroll",
+            padding: 20,
+            backgroundColor: "#fefefe",
+            boxShadow: "0 0 8px gray",
+          }}
+        >
+          <DebugCart style={{}} />
+        </div>
+      ) : null}
+    </CartProvider>
+  );
 }
+
+export default MyApp;
